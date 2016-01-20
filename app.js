3c31673a9cf2c9c46e358b1862b57745d31e4047
shaolong <shaolonglin09@gmail.com>
2016-01-19 21:20:40 -0500

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+var main = function(){
+  $('.entypo-menu').click(function(){
+      $('.side-menu').animate({
+        left: "0px"
+      },200);
+
+      $("body").animate({
+        left: "250px"
+      },200);
+  });
+
+  $(".entypo-left-open-big").click(function(){
+     $('.side-menu').animate({
+        left: "-250px"
+      },200);
+
+      $("body").animate({
+        left: "0px"
+      },200);
+
+  });
+
+};
+
+
+$(document).ready(main);
